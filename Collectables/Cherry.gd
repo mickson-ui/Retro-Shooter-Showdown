d246400ba9e4f638ab42b992b77cf64e9d738acc
Mickson Bonsu Bossman <mbonsu69@outlook.com>
2024-06-04 10:12:34 +0200

    First commit

---
+++ Collectables/Cherry.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		Game.Gold += 5
+		var tween = get_tree().create_tween()
+		var tween1 = get_tree().create_tween()
+		
+		tween.tween_property(self, "position", position - Vector2(0,35), 0.35)
+		tween.tween_property(self, "modulate:a", 0, 0.3)
+		tween.tween_callback(queue_free)
---
+++ Frog.gd
@@ -0,0 +1,60 @@
+extends CharacterBody2D
+
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var SPEED = 100
+var player
+var chase = false
+
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+	
+func _physics_process(delta):
+	#Frog GRavity
+	velocity.y += gravity * delta
+	
+	if chase == true:
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Jump")
+		player = get_node("../../Player/Player")
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h= true
+		else:
+			get_node("AnimatedSprite2D").flip_h = false
+		velocity.x = direction.x * SPEED
+	else:
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Idle")
+		velocity.x = 0
+		
+	move_and_slide()
+
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		chase = true
+		
+		
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		chase = false
+
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		death()
+
+
+func _on_player_collision_body_entered(body):
+	if body.name == "Player":
+		Game.playerHP -= 3
+		death()
+	
+	
+func death():
+	Game.Gold += 5
+	Utils.saveGame()
+	chase = false
+	get_node("AnimatedSprite2D").play("Death")
+	await get_node("AnimatedSprite2D").animation_finished
+	self.queue_free()
